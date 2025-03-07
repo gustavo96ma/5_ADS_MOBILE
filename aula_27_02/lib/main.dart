239749d584a5d1eb534f1128da791b8f6d3cec8e
Gustavo <gustavo96ma@gmail.com>
2025-03-06 22:12:10 -0300

    06/03

--- aula_27_02/lib/main.dart
+++ aula_27_02/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await Supabase.initialize(
+    url: 'https://fhjubwtbyugrehqxuzef.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoanVid3RieXVncmVocXh1emVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEzMDgxMzAsImV4cCI6MjA1Njg4NDEzMH0.xWnTF7mppLOUkwaAY08dc4Pe3vng68qtk-_JGO396e0',
+  );
   runApp(const MainApp());
 }
 
@@ -22,7 +27,7 @@ class MainApp extends StatelessWidget {
                 width: 200,
               ),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 80),
+                        padding: const EdgeInsets.fromLTRB(20, 20, 20, 5),
                 child: Card(
                   color: Color(0xff84996c),
                   child: Column(
@@ -30,7 +35,7 @@ class MainApp extends StatelessWidget {
                     spacing: 10,
                     children: [
                       Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        padding: const EdgeInsets.all(20),
                         child: TextField(
                           decoration: InputDecoration(
                             border: OutlineInputBorder(
@@ -42,7 +47,7 @@ class MainApp extends StatelessWidget {
                         ),
                       ),
                       Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        padding: const EdgeInsets.fromLTRB(20, 5, 20, 20),
                         child: TextField(
                           decoration: InputDecoration(
                             border: OutlineInputBorder(
