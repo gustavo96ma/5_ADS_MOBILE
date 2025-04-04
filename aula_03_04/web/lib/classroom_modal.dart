0a4f4e0d282b2d4ac8235935ed5196d90d58d97d
gustavo <gustavo96ma@gmail.com>
2025-04-03 22:20:28 -0300

    03/04

--- aula_03_04/web/lib/classroom_modal.dart
+++ aula_03_04/web/lib/classroom_modal.dart
@@ -18,10 +18,10 @@ class _ClassroomModalState extends State<ClassroomModal> {
       insetPadding: EdgeInsets.only(left: 100, bottom: 50),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
       child: Container(
-        height: 300.0,
-        width: 600.0,
+        height: 500.0,
+        width: 550.0,
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20),
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
           child: Form(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
@@ -38,161 +38,240 @@ class _ClassroomModalState extends State<ClassroomModal> {
                     icon: Icon(Icons.close, size: 16),
                   ),
                 ),
-                Row(
-                  spacing: 20,
-                  children: [
-                    SizedBox(
-                      width: 150,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          label: Text(
-                            'Nome',
-                            style: TextStyle(
-                              color: Color(0xff2b4612),
-                              fontSize: 16,
-                            ),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xff2b4612)),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xff2b4612)),
-                          ),
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xff2b4612)),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 150,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          label: Text(
-                            'Capacidade',
-                            style: TextStyle(
-                              color: Color(0xff2b4612),
-                              fontSize: 16,
-                            ),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xff2b4612)),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xff2b4612)),
-                          ),
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xff2b4612)),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 150,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          label: Text(
-                            'Computadores',
-                            style: TextStyle(
-                              color: Color(0xff2b4612),
-                              fontSize: 16,
-                            ),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xff2b4612)),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xff2b4612)),
-                          ),
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xff2b4612)),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-
-                Row(
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          'Projetor',
-                          style: TextStyle(
-                            color: Color(0xff2b4612),
-                            fontSize: 16,
-                          ),
-                        ),
-                        Transform.scale(
-                          scale: 0.6,
-                          child: Switch(
-                            value: projector,
-                            activeColor: Color(0xff2b4612),
-                            onChanged: (bool value) {
-                              setState(() {
-                                projector = value;
-                              });
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          'TV',
-                          style: TextStyle(
-                            color: Color(0xff2b4612),
-                            fontSize: 16,
-                          ),
-                        ),
-                        Transform.scale(
-                          scale: 0.6,
-                          child: Switch(
-                            value: television,
-                            activeColor: Color(0xff2b4612),
-                            onChanged: (bool value) {
-                              setState(() {
-                                television = value;
-                              });
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          'Ar condicionado',
-                          style: TextStyle(
-                            color: Color(0xff2b4612),
-                            fontSize: 16,
-                          ),
-                        ),
-                        Transform.scale(
-                          scale: 0.6,
-                          child: Switch(
-                            value: airConditioning,
-                            activeColor: Color(0xff2b4612),
-                            onChanged: (bool value) {
-                              setState(() {
-                                airConditioning = value;
-                              });
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-
-                TextButton(
-                  onPressed: () {},
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
                   child: Row(
-                    children: [Icon(Icons.save_outlined), Text('Salvar')],
+                    spacing: 20,
+                    children: [
+                      SizedBox(
+                        width: 150,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            label: Text(
+                              'Nome',
+                              style: TextStyle(
+                                color: Color(0xff2b4612),
+                                fontSize: 16,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xff2b4612)),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xff2b4612)),
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xff2b4612)),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 150,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            label: Text(
+                              'Capacidade',
+                              style: TextStyle(
+                                color: Color(0xff2b4612),
+                                fontSize: 16,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xff2b4612)),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xff2b4612)),
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xff2b4612)),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 150,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            label: Text(
+                              'Computadores',
+                              style: TextStyle(
+                                color: Color(0xff2b4612),
+                                fontSize: 16,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xff2b4612)),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xff2b4612)),
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xff2b4612)),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Row(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            'Projetor',
+                            style: TextStyle(
+                              color: Color(0xff2b4612),
+                              fontSize: 16,
+                            ),
+                          ),
+                          Transform.scale(
+                            scale: 0.6,
+                            child: Switch(
+                              value: projector,
+                              activeColor: Color(0xff2b4612),
+                              onChanged: (bool value) {
+                                setState(() {
+                                  projector = value;
+                                });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            'TV',
+                            style: TextStyle(
+                              color: Color(0xff2b4612),
+                              fontSize: 16,
+                            ),
+                          ),
+                          Transform.scale(
+                            scale: 0.6,
+                            child: Switch(
+                              value: television,
+                              activeColor: Color(0xff2b4612),
+                              onChanged: (bool value) {
+                                setState(() {
+                                  television = value;
+                                });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            'Ar condicionado',
+                            style: TextStyle(
+                              color: Color(0xff2b4612),
+                              fontSize: 16,
+                            ),
+                          ),
+                          Transform.scale(
+                            scale: 0.6,
+                            child: Switch(
+                              value: airConditioning,
+                              activeColor: Color(0xff2b4612),
+                              onChanged: (bool value) {
+                                setState(() {
+                                  airConditioning = value;
+                                });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Row(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            'Carteira p/ canhoto',
+                            style: TextStyle(
+                              color: Color(0xff2b4612),
+                              fontSize: 16,
+                            ),
+                          ),
+                          Transform.scale(
+                            scale: 0.6,
+                            child: Switch(
+                              value: airConditioning,
+                              activeColor: Color(0xff2b4612),
+                              onChanged: (bool value) {
+                                setState(() {
+                                  airConditioning = value;
+                                });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            'Carteira p/ PCD',
+                            style: TextStyle(
+                              color: Color(0xff2b4612),
+                              fontSize: 16,
+                            ),
+                          ),
+                          Transform.scale(
+                            scale: 0.6,
+                            child: Switch(
+                              value: airConditioning,
+                              activeColor: Color(0xff2b4612),
+                              onChanged: (bool value) {
+                                setState(() {
+                                  airConditioning = value;
+                                });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+
+                SizedBox(
+                  width: 100,
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(
+                        Color(0xff2b4612),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Row(
+                      spacing: 8,
+                      children: [
+                        Icon(Icons.save_outlined, color: Colors.white),
+                        Text('Salvar', style: TextStyle(color: Colors.white)),
+                      ],
+                    ),
+                  ),
+                ),
+                ListView.builder(itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('Teste'),
+                    tileColor: Colors.grey,
+                  
+                  );
+                },
+                itemCount: ,),
               ],
             ),
           ),
