988dad9485bd854149bf1e12afe8d0c79649914e
Gustavo <gustavo96ma@gmail.com>
2025-05-15 22:14:38 -0300

    15/05

---
+++ aula_15_05/web/lib/models/classroom_model.dart
@@ -0,0 +1,23 @@
+class ClassroomModel {
+  int id;
+  String classroom;
+  int capacity;
+  int computersCount;
+  bool hasProjector;
+  bool hasTelevision;
+  bool hasAirConditioning;
+  bool hasLeftHandedDesk;
+  bool hasHandicapDesk;
+
+  ClassroomModel({
+    required this.classroom,
+    required this.capacity,
+    required this.computersCount,
+    required this.hasProjector,
+    required this.hasTelevision,
+    required this.hasAirConditioning,
+    required this.hasLeftHandedDesk,
+    required this.hasHandicapDesk,
+    required this.id,
+  });
+}
